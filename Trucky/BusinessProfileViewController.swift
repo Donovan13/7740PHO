2b1b663f69096158ea69f17c12a6343ef5091c8e
Donovan13 <kdbouchard@gmail.com>
2016-09-09 16:33:58 -0400

    updated detail page

--- Trucky/BusinessProfileViewController.swift
+++ Trucky/BusinessProfileViewController.swift
@@ -8,17 +8,22 @@
 
 import UIKit
 import Firebase
+import CoreLocation
 
 class BusinessProfileViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var ref:FIRDatabaseReference!
     var businesses = [Business]()
-    
-    
     var truckName:String?
     var trucks: Truck!
+    var userlocation: CLLocation?
+    
     
     let userDefaults = NSUserDefaults.standardUserDefaults()
+    
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,13 +40,35 @@ class BusinessProfileViewController: UIViewController, UITableViewDelegate, UITa
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let tableImage = UIImage (named: "tacos")
+        let iconFrame = UIImageView(image: tableImage)
+        //        iconFrame.contentMode = .ScaleAspectFit
+        //        if iconFrame.bounds.size.width > (UIImage).size.width && iconFrame.bounds.size.height > (UIImage).size.height {
+        //        iconFrame.contentMode = UIViewContentMode.ScaleAspectFit
+        //        }
+        tableView.backgroundView = iconFrame
+        
+        tableView.backgroundColor = UIColor .clearColor()
+        
         if indexPath.row == 0 {
-
-            let cell = tableView.dequeueReusableCellWithIdentifier("titleSegue", forIndexPath: indexPath) 
             
-//            cell.textLabel?.text = trucks.truckName
-            cell.detailTextLabel?.text = trucks.truckName
+            let cell = tableView.dequeueReusableCellWithIdentifier("titleSegue") as! DetailTableViewCell
+            let location = CLLocation(latitude: trucks.latitude!, longitude: trucks.longitude!)
             
+            
+            cell.truckNameLabel?.text = trucks.truckName?.capitalizedString
+            cell.reviewsLabel?.text = "\(trucks.reviewCount!) reviews on Yelp"
+            cell.ratingsImageView.image = UIImage(data: NSData(contentsOfURL: NSURL(string:trucks.ratingImageURL!)!)!)!
+            
+            if userlocation != nil {
+                let distance = location.distanceFromLocation(userlocation!)
+                let inMiles = distance * 0.000621371192
+                cell.distanceLabel.text = (String(format: "%.2fm Away", inMiles))
+            }
+            
+            
+            cell.backgroundColor! = UIColor.clearColor()
             return cell
         }
         else if indexPath.row == 1 {
@@ -51,7 +78,7 @@ class BusinessProfileViewController: UIViewController, UITableViewDelegate, UITa
         }
         else if indexPath.row == 2 {
             let cell = tableView.dequeueReusableCellWithIdentifier("websiteSegue", forIndexPath: indexPath)
-//            cell.detailTextLabel?.text = trucks.website
+            cell.detailTextLabel?.text = trucks.website
             return cell
         }
         else if indexPath.row == 3 {
@@ -61,12 +88,12 @@ class BusinessProfileViewController: UIViewController, UITableViewDelegate, UITa
         }
         else if indexPath.row == 4 {
             let cell = tableView.dequeueReusableCellWithIdentifier("hoursSegue", forIndexPath: indexPath)
-//            cell.detailTextLabel?.text = trucks.hours
+            //            cell.detailTextLabel?.text = trucks.hours
             return cell
         }
         else if indexPath.row == 5 {
             let cell = tableView.dequeueReusableCellWithIdentifier("categorySegue", forIndexPath: indexPath)
-//            cell.detailTextLabel?.text = trucks.category
+            //            cell.detailTextLabel?.text = trucks.category
             return cell
         }
         else if indexPath.row == 6 {
@@ -82,6 +109,13 @@ class BusinessProfileViewController: UIViewController, UITableViewDelegate, UITa
             return cell
         }
     }
-
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        if indexPath.row == 0 {
+            
+            return 120
+        } else {
+            return 50
+    }
+    }
     
 }
---
+++ Trucky/DetailTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  DetailTableViewCell.swift
+//  Trucky
+//
+//  Created by Kyle on 9/9/16.
+//  Copyright © 2016 Kyle. All rights reserved.
+//
+
+import UIKit
+
+class DetailTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var truckNameLabel: UILabel!
+    @IBOutlet weak var ratingsImageView: UIImageView!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var reviewsLabel: UILabel!
+
+}
