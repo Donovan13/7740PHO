7a90fea24c5b49f3223cee085863d523c04f9fe0
Mingu Chu <mingu@Mingus-MacBook-Pro.local>
2016-10-03 12:57:07 -0500

    middle of yelp fusion

--- Trucky/YelpAPIFusion.swift
+++ Trucky/YelpAPIFusion.swift
@@ -1,41 +1,42 @@
-////
-////  YelpAPIFusion.swift
-////  Trucky
-////
-////  Created by Mingu Chu on 9/27/16.
-////  Copyright © 2016 Kyle. All rights reserved.
-////
 //
-//import Foundation
-//import OAuthSwift
+//  YelpAPIFusion.swift
+//  Trucky
+//
+//  Created by Mingu Chu on 9/27/16.
+//  Copyright © 2016 Kyle. All rights reserved.
+//
+
+import Foundation
+import OAuthSwift
+
+let grantType = "client_credentials"
+let accessTokenUrl = "https://api.yelp.com/oauth2/token"
+
+
+
+class YelpAPIFusion: OAuth2Swift {
+        class var sharedInstance: YelpAPIFusion {
+            struct Static {
+                static var instance: YelpAPIFusion!
+                static var token: dispatch_once_t = 0
+            }
+            return Static.instance
+        }
+    
+    func getToken() -> String {
+        let yelpParameters = [
+            "grant_type" : "client_credentials",
+            "client_id": "bDmceefKP77HVqMmDkreWA",
+            "client_secret": "fHyROuLUncM4GvpoIEI2Z4mKEVEtErvBF83wpivqA1zCAgJxcVWWQfpGpU78kGHy"]
+        let requestToken = client.post(accessTokenUrl, parameters: yelpParameters, headers: nil, success:  , failure: nil)
+
+    }
+    
+}
+
+//let accessToken = "https://api.yelp.com/oauth2/token"
+//
+
+//let oAuth2Swift = OAuth2Swift(parameters: yelpParameters)
 //
 //
-//let consumerKey = "bDmceefKP77HVqMmDkreWA"
-//let consumerSecret = "fHyROuLUncM4GvpoIEI2Z4mKEVEtErvBF83wpivqA1zCAgJxcVWWQfpGpU78kGHy"
-//let grantType = "client_credentials"
-//let accessTokenUrl = "https://api.yelp.com/oauth2/token"
-//
-//
-//class YelpAPIFusion: OAuth2Swift {
-//    
-//    
-//    
-////    class var sharedInstance: YelpAPIFusion {
-////        struct Static {
-////            static var instance: YelpAPIFusion!
-////            static var token: dispatch_once_t = 0
-////        }
-////        dispatch_once(&Static.token) {
-////            Static.instance = YelpAPIFusion
-////        }
-////    }
-//    
-//    
-//    
-//    
-//    
-//
-//    
-//    
-//    
-//}
